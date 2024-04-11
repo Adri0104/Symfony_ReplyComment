37c12ecc3ad5d042c6f4bbc1b33c3981a4998d11
Adrien <adrien@mbp-de-adrien.home>
2024-04-11 18:43:01 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(Request $request, EntityManagerInterface $entityManager, CommentRepository $commentRepository): Response
+    {
+        $comments = $commentRepository->findAll();
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
+            'comments' => $comments,
+        ]);
+    }
+}
